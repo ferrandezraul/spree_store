e35fc72766ec1f2e9d6d356fa272614cf6f902b5
Raul Ferrandez <ferrandezraul@gmail.com>
2014-05-30 19:00:45 +0200

    Added PaymentMethod check. Fixed default shipping methods. Fixed some catalan translations

---
+++ db/default/shipping_methods.rb
@@ -0,0 +1,36 @@
+load "#{Rails.root}/db/default/zones.rb"
+
+puts "Loading Shipping Methods ..."
+
+begin
+eu_zone = Spree::Zone.find_by_name!("EU_VAT")
+rescue ActiveRecord::RecordNotFound
+  puts "Couldn't find 'EU_VAT' zone. Did you run `rake db:seed` first?"
+  puts "That task will set up the countries, states and zones required for Spree."
+  exit
+end
+
+shipping_category = Spree::ShippingCategory.find_or_create_by!(:name => 'Ecocity a domicilio')
+
+Spree::ShippingMethod.create!(
+    :name => "Ecocity a domicili Flat Rate",
+    #:zones => [eu_zone],
+    :tax_category_id => Spree::TaxCategory.find_or_create_by!(:name => "General").id,
+    :calculator => Spree::Calculator::Shipping::FlatRate.create!,
+    :shipping_categories => [shipping_category]
+)
+
+{
+  "Ecocity a domicili Flat Rate" => [6, "EUR"]
+}.each do |shipping_method_name, (price, currency)|
+  shipping_method = Spree::ShippingMethod.find_by_name!(shipping_method_name)
+  shipping_method.calculator.preferred_amount = price
+  shipping_method.calculator.preferred_currency = currency
+  shipping_method.save!
+
+  # cause current spree causes an exception trying to create a ShippinMethod with :zones list
+  # I need to do it afterwards
+  # https://github.com/spree/spree/issues/4524
+  eu_zone.shipping_methods << shipping_method
+end
+
