d9185eff1befbb7d968fd66b32f8917c50c0e4c1
Raul Ferrandez <ferrandezraul@gmail.com>
2014-06-04 18:09:11 +0200

    Added minimum of 100 EUR to proceed with order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,17 +1,14 @@
 
 def minimum_amount_added_to_cart?
 
-  unless amount > 100
+  unless amount > 99
     errors.add(:base, Spree.t(:minimum_amount_of_hundred_euros)) and return false
   end
 
-  #unless line_items.present?
-  #  errors.add(:base, t(:minimum_amount_of_hundred_euros)) and return false
-  #end
-
 end
 
-
+Spree::Order.state_machine.before_transition :from => :cart, :do => :minimum_amount_added_to_cart?
 Spree::Order.state_machine.before_transition :to => :delivery, :do => :minimum_amount_added_to_cart?
+
 
 
