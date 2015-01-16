da29f1898bc41a52bafe3e9952ff2c1943b737e5
Raul Ferrandez <ferrandezraul@gmail.com>
2015-01-16 21:36:26 +0100

    foo

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,17 @@
 
 require 'cucumber/rails'
 
+require 'capybara'
+require 'capybara/cucumber'
+
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/webkit/matchers'
+
+Capybara.javascript_driver = :webkit
+Capybara.current_driver = :webkit
+Capybara.asset_host = "http://localhost:3000"
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
