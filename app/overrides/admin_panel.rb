dafb9855c2a6889f7712c8adedd3e26c6d5dc68d
Raul Ferrandez <ferrandezraul@gmail.com>
2015-03-12 00:18:23 +0100

    Make link to export products as CSV is done

--- app/overrides/admin_panel.rb
+++ app/overrides/admin_panel.rb
@@ -1,4 +1,9 @@
+#
+# Create a link in admin panel to download products as CSV.
+#
 Deface::Override.new(:virtual_path  => "spree/admin/products/index", #/index.html.erb 
-                     :insert_before => "li#new_product_link",
-                     :text          => "<p>Export products to CSV!</p>",
+                     :insert_after => "li#new_product_link",
+                     :text          => "<%= link_to 'Export Products as CSV', 
+                     								 products_path( format: 'csv' ), 
+                     								 { :style => 'padding:15px 15px;'} %>", # GET products as CSV
                      :name          => "registration_future")
