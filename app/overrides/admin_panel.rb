f5ef7d53d2ff9c93e42f4d1c1690666eacd41e9f
Raul Ferrandez <ferrandezraul@gmail.com>
2015-04-25 19:30:05 +0200

    foo

--- app/overrides/admin_panel.rb
+++ app/overrides/admin_panel.rb
@@ -3,8 +3,7 @@
 #
 Deface::Override.new(:virtual_path  => "spree/admin/products/index", #/index.html.erb 
                      :insert_after => "li#new_product_link",
-                     :text          => "<%= link_to 'Export Products as CSV', 
-                     								 products_path( format: 'csv' ), 
+                     :text          => "<%= link_to 'Export Products as CSV', products_path( format: 'csv' ),
                      								 { :style => 'padding:15px 15px;'} %>", # GET products as CSV
                      :name          => "export_products_as_csv")
 
@@ -14,8 +13,14 @@ Deface::Override.new(:virtual_path  => "spree/admin/products/index", #/index.htm
 #
 Deface::Override.new(:virtual_path  => "spree/admin/products/index", #/index.html.erb 
                      :insert_after => "li#new_product_link",
-                     :text          => "<%= form_tag import_admin_products_path, multipart: true do %>
-										  <%= file_field_tag :file %>
-										  <%= submit_tag 'Import Products from CSV' %>
-										<% end %>", # POST products as CSV
+                     :text          => "<fieldset>
+                                          <%= form_tag import_admin_products_path, multipart: true do %>
+                                            <p>
+                                              <%= file_field_tag :file %>
+                                            </p>
+                                            <p>
+                                              <%= submit_tag 'Import Products from CSV' %>
+                                            </p>
+										                      <% end %>
+                                        </fieldset>", # POST products as CSV
                      :name          => "import_products_from_csv")
