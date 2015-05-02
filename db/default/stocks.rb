b0e5146d205aa520e0d32317ffd6ce92e564c447
Raul Ferrandez <ferrandezraul@gmail.com>
2015-05-02 21:07:52 +0200

    Added default stock of 1000 for all products in seed data

---
+++ db/default/stocks.rb
@@ -0,0 +1,10 @@
+country =  Spree::Country.find_by( iso: 'ES')
+location = Spree::StockLocation.first_or_create! name: 'default', address1: 'Ecocity Street', city: 'Riudaura', zipcode: '12345', country: country, state: country.states.first
+location.active = true
+location.save!
+
+Spree::Variant.all.each do |variant|
+  variant.stock_items.each do |stock_item|
+    Spree::StockMovement.create( :quantity => 1000, :stock_item => stock_item )
+  end
+end
