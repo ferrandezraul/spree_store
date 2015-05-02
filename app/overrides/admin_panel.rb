985494895003b9758ad1b252fa1b094896b46aec
Raul Ferrandez <ferrandezraul@gmail.com>
2015-05-02 19:40:54 +0200

    Add deface customizations in admin panel. Added import and export CSV files

--- app/overrides/admin_panel.rb
+++ app/overrides/admin_panel.rb
@@ -1,30 +1,30 @@
 #
 # Create a link in admin panel to download products as CSV.
 #
-#Deface::Override.new(:virtual_path  => "spree/admin/products/index", #/index.html.erb
-#	:name          => "admin_panel",
-#	:insert_after => "h1",
-#    # Note that here I have to change the method to GET since a button uses POST by default
-#    :text          => "<%= button_to 'Export Products as CSV', products_path( format: 'csv' ), :class => 'button', :method => :get  %>") # GET products as CSV
-
+Deface::Override.new( :virtual_path => "spree/layouts/admin",
+                      :name => "admin_export_products",
+                      :insert_bottom => "[data-hook='admin_tabs']",
+                      :text => "<%= button_to 'Export Products as CSV', products_path( format: 'csv' ), :class => 'button', :method => :get  %>",
+                      :disabled => false )
 
 #
 # Create a link in admin panel to import products from CSV.
 #
-# Deface::Override.new(:virtual_path  => "spree/admin/products/index", #/index.html.erb
-#                      :insert_after => "li#new_product_link",
-#                      :text          => "<fieldset>
-#                                           <%= form_tag import_admin_products_path, multipart: true do %>
-#                                             <p>
-#                                               <%= file_field_tag :file %>
-#                                             </p>
-#                                             <p>
-#                                               <%= submit_tag 'Import Products from CSV',
-#                                                               :disabled => true,
-#                                                               data: {
-#                                                                 :confirm => 'This will erase all your products. Are you sure you want to continue?'
-#                                                               } %>
-#                                             </p>
-# 										                      <% end %>
-#                                         </fieldset>", # POST products as CSV
-#                      :name          => "import_products_from_csv")
+Deface::Override.new( :virtual_path => "spree/layouts/admin",
+                      :name => "admin_import_products",
+                      :insert_bottom => "[data-hook='admin_tabs']",
+                      :text => "<fieldset>
+                                  <%= form_tag import_admin_products_path, multipart: true do %>
+                                    <p>
+                                      <%= file_field_tag :file %>
+                                    </p>
+                                    <p>
+                                      <%= submit_tag 'Import Products from CSV',
+                                                     :disabled => true,
+                                                     data: {
+                                                             :confirm => 'This will erase all your products. Are you sure you want to continue?'
+                                                           } %>
+                                    </p>
+                                  <% end %>
+                                </fieldset>", # POST products as CSV
+                      :disabled => false )
