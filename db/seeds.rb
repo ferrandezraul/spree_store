c13822785fd4fd4ab1200225804e09cd33d70f83
Raul Ferrandez <ferrandezraul@gmail.com>
2015-05-03 19:46:34 +0200

    Added admin user in seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,19 @@
 
 
 #Spree::Core::Engine.load_seed if defined?(Spree::Core)    # Do not seed data from spree core
-Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+#Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 # Loads seed data under my default dir (my own countries, roles, states, tax_rates, etc .. )
 default_path = File.join(File.dirname(__FILE__), 'default')
 
 Rake::Task['db:load_dir'].reenable
 Rake::Task['db:load_dir'].invoke(default_path)
+
+user = Spree::User.create!( :email => ENV['HEROKU_STORE_EMAIL'],
+                            :login => ENV['HEROKU_STORE_LOGIN'],
+                            :password => ENV['HEROKU_STORE_PASSWORD'] )
+
+user.spree_roles << Spree::Role.find_or_create_by!( name: "admin" )
 
 
 
